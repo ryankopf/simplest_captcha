34d174fb143bffba5ef1d111e98b181cb59ee718
ryankopf <ryan@ryankopf.com>
2021-12-05 16:44:08 -0600

    Creating initial functions.

--- lib/simplest_captcha/captcha_helper.rb
+++ lib/simplest_captcha/captcha_helper.rb
@@ -11,7 +11,7 @@ module SimplestCaptcha
       stringbuf << f.hidden_field(:captcha_id, value: "#{captcha_public}")
       stringbuf << "<div class='cimg'><img src='/captcha/#{captcha_public}.gif' id='captcha_img' class='captcha_img'></div>"
       img = "<img src='https://i.ani.me/0251/9977/ref.png' alt='new image' border='0' class='refresh'>"
-      stringbuf << link_to(img.html_safe, {controller: "captcha", action: "show", id: captcha_public, format: "js" }, method: :get, remote: true)
+      stringbuf << link_to(img.html_safe, "/captcha/#{captcha_public.id}.js", method: :get, remote: true)
       stringbuf << "<div>"
       stringbuf << f.text_field(:captcha, autocomplete: :off)
       stringbuf << "</div>"
@@ -19,7 +19,7 @@ module SimplestCaptcha
         stringbuf << "<div class='captcha_hint'>"
         if logged_in?
           stringbuf << "Repeat the letters shown or "
-          stringbuf << link_to("validate your mobile phone.", current_user, target: "_blank")
+          stringbuf << link_to("validate your mobile phone.", "/users/#{current_user.id}", target: "_blank")
         else
           stringbuf << "<a href='/login' target='_blank'>Login and validate your account.</a>"
         end
