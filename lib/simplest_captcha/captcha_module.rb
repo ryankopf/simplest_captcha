26aac7f9131f6dd901f0a23cddd6fcc400ef76e4
ryankopf <ryan@ryankopf.com>
2021-12-06 18:07:58 -0600

    Creating initial functions.

--- lib/simplest_captcha/captcha_module.rb
+++ lib/simplest_captcha/captcha_module.rb
@@ -12,6 +12,11 @@ module SimplestCaptcha
       errors.add("captcha","does not match the letters pictured.") unless SimplestCaptcha::Captcha.valid(self)
     end
 
+    def captcha_valid_on_new_record
+      return unless new_record?
+      captcha_valid
+    end
+
     def captcha_valid_for_revisable
       return true if (self.revision_of.present? && self.new_record?)
       return true if self.captcha_skip
